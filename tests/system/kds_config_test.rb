2c66bf147f6c12d52a7aa3f7d29b39cc395e1ffa
Aditya <56039219+adijr9487@users.noreply.github.com>
2024-06-28 00:50:19 +0530

    [dev] - Add KDS system test (#107)
    Co-authored-by: Vivek <vivekmiyaniofficial@gmail.com>

---
+++ tests/system/kds_config_test.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class KdsConfigTest < ApplicationSystemTestCase
+  test "change restaurant" do
+    restaurant = create(:restaurant, name: "Restaurant 1")
+    another_restaurant = create(:restaurant, name: "Restaurant 2")
+    kds_admin = create(:admin, restaurants: [restaurant, another_restaurant])
+    kitchen_profile = create(:kitchen_profile, name: "Kitchen Profile 1", restaurant: restaurant)
+
+    kds_sign_in(kds_admin, restaurant, kitchen_profile)
+
+    visit path_for(:frontend, "/kds")
+    wait_for_pending_requests
+
+    find(".anticon-menu").click
+
+    within ".ant-drawer" do
+      find(".ant-collapse-header", text: "Account").click
+      wait_for_pending_requests
+
+      within ".ant-collapse-content-active" do
+        fill_in_select with: "Restaurant 2"
+      end
+    end
+
+    within ".ant-modal" do
+      assert_selector ".ant-modal-title", text: "Admin Verification"
+
+      fill_in "Email", with: kds_admin.email
+      fill_in "Password", with: kds_admin.password
+
+      click_on "Submit"
+      wait_for_pending_requests
+    end
+
+    within ".ant-drawer" do
+      within ".ant-collapse-content-active" do
+        assert_text "Restaurant 2"
+      end
+    end
+  end
+end
--- tests/test_helpers/sessions_helper.rb
+++ tests/test_helpers/sessions_helper.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 module SessionsHelper
+  def kds_sign_in(user, restaurant, kitchen_profile)
+    hash = { state: { token: Session.token(user, "kds_admin_id") }, version: 0 }
+    config = { state: { bookingTypes: [], kitchenProfileId: kitchen_profile.id, restaurantId: restaurant.id },
+               version: 0 }
+
+    visit path_for(:frontend, "/kds")
+    page.execute_script "window.localStorage.setItem('kds-session-store', arguments[0]);", hash.to_json
+    page.execute_script "window.localStorage.setItem('kitchen-config-store', arguments[0]);", config.to_json
+  end
+
   def sign_in(user)
     hash = { state: { token: Session.token(user, "web_admin_id") }, version: 0 }
 
