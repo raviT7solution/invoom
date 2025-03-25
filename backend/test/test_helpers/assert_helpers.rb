8d7e3c61a41dda65c89623bfa0266a46c53c9cd4
Vivek <vivekmiyaniofficial@gmail.com>
2025-03-26 01:56:39 +0530

    [dev] - Fix incorrect assert attribute checks (#230)

--- backend/test/test_helpers/assert_helpers.rb
+++ backend/test/test_helpers/assert_helpers.rb
@@ -4,7 +4,7 @@ module AssertHelpers
   def assert_attributes(record, hash)
     hash.each do |k, v|
       if v.nil?
-        assert_nil v
+        assert_nil record.public_send(k)
       else
         assert_equal v, record.public_send(k)
       end
