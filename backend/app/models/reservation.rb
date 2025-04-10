b5ef5af1065627efd505bdd5e7f99960c555b927
rsoniERPBuddy <147432928+rsoniERPBuddy@users.noreply.github.com>
2025-04-10 17:36:29 +0530

    PLATTER-422 - Send sms on reservation update (#232)
    Co-authored-by: Vivek <vivekmiyaniofficial@gmail.com>

--- backend/app/models/reservation.rb
+++ backend/app/models/reservation.rb
@@ -11,11 +11,12 @@ class Reservation < ApplicationRecord
   validates :reservation_at, presence: true
   validates :status, presence: true
 
-  after_create_commit :send_sms, if: ->(i) { i.restaurant.twilio_configured? }
+  after_create_commit :send_create_sms, if: ->(i) { i.restaurant.twilio_configured? }
+  after_update_commit :send_update_sms, if: ->(i) { i.restaurant.twilio_configured? }
 
   private
 
-  def send_sms # rubocop:disable Metrics/AbcSize
+  def send_create_sms # rubocop:disable Metrics/AbcSize
     reservation_at_tz = reservation_at.in_time_zone(restaurant.timezone)
 
     body = <<~BODY
@@ -25,6 +26,24 @@ class Reservation < ApplicationRecord
       Guests: #{adults + kids}
       We look forward to serving you!
       If you need to modify or cancel, call us at #{restaurant.phone_number}.
+      See you soon!
+    BODY
+
+    TwilioJob.perform_later(restaurant, body: body, to: customer.phone_number_in_e164)
+  end
+
+  def send_update_sms # rubocop:disable Metrics/AbcSize
+    return unless saved_changes.keys.intersect?(["customer_id", "adults", "kids", "reservation_at"])
+
+    reservation_at_tz = reservation_at.in_time_zone(restaurant.timezone)
+
+    body = <<~BODY
+      Dear Customer, your reservation at #{restaurant.name} has been updated!
+      Date: #{reservation_at_tz.strftime('%m-%d-%Y')}
+      Time: #{reservation_at_tz.strftime('%I:%M %p')}
+      Guests: #{adults + kids}
+      We look forward to serving you!
+      If you need to make further changes or cancel, please call us at #{restaurant.phone_number}.
       See you soon!
     BODY
 
